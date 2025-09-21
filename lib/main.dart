b885128e4b3aa9e1a6806a9160944d0834146b0c
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2025-09-22 09:34:48 +1000

    Add localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:leeft/l10n/app_localizations.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,8 +11,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),

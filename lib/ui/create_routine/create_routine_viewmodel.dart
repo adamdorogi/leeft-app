347aa3887344e800b142d50a40913789c5a8964b
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2025-12-19 19:17:00 +1000

    Add more logging

--- lib/ui/create_routine/create_routine_viewmodel.dart
+++ lib/ui/create_routine/create_routine_viewmodel.dart
@@ -9,6 +9,8 @@ import 'package:leeft/domain/models/routine_exercise/routine_exercise.dart';
 import 'package:leeft/utils/command.dart';
 import 'package:leeft/utils/result.dart';
 
+import 'package:logging/logging.dart';
+
 /// A view model for managing the UI state of the routine creation screen.
 class CreateRoutineViewModel extends ChangeNotifier {
   /// Creates a [CreateRoutineViewModel] with an [exerciseRepository].
@@ -16,6 +18,8 @@ class CreateRoutineViewModel extends ChangeNotifier {
   /// The [exerciseRepository] retrieves the exercises and stores the routine.
   CreateRoutineViewModel({required ExerciseRepository exerciseRepository})
     : _exerciseRepository = exerciseRepository;
+
+  final _log = Logger((CreateRoutineViewModel).toString());
 
   final ExerciseRepository _exerciseRepository;
 
@@ -33,6 +37,7 @@ class CreateRoutineViewModel extends ChangeNotifier {
     if (exerciseIds == null || exerciseIds.isEmpty) {
       return Result.success(null);
     }
+    _log.info('Adding ${exerciseIds.length} exercises to routine...');
     final exerciseResults = await Future.wait(
       exerciseIds.map(_exerciseRepository.exerciseWith),
     );
@@ -44,6 +49,7 @@ class CreateRoutineViewModel extends ChangeNotifier {
         .toList();
     final newRoutineExercises = _routine.routineExercises + routineExercises;
     _routine = _routine.copyWith(routineExercises: newRoutineExercises);
+    _log.info('Successfully added ${exerciseIds.length} exercises to routine.');
     return Result.success(null);
   }
 }

19a6d9deb6a6616c5e2b46be0463d1b0f0ff25a6
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2026-03-01 17:29:45 +1000

    Remove `google_fonts` package
    - Fetched Inter font is different to one listed on website
    - Prefer using local asset instead

--- lib/ui/core/themes.dart
+++ lib/ui/core/themes.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'package:google_fonts/google_fonts.dart';
-
 /// A collection of themes.
 abstract final class Themes {
   /// The theme for the light [ThemeMode].
-  static final lightTheme = ThemeData(
-    textTheme: GoogleFonts.interTextTheme(),
-  );
+  static final lightTheme = ThemeData();
 
   /// The theme for the dark [ThemeMode].
-  static final darkTheme = ThemeData(
-    brightness: .dark,
-    textTheme: GoogleFonts.interTextTheme(ThemeData.dark().textTheme),
-  );
+  static final darkTheme = ThemeData(brightness: .dark);
 }

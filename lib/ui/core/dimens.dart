820b76ae2428494da850e07943935182c4a06044
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2026-03-15 18:18:29 +1000

    Update thumbnail size
    Update thumbnail size to fill `ListTile`

--- lib/ui/core/dimens.dart
+++ lib/ui/core/dimens.dart
@@ -7,7 +7,7 @@ abstract final class Dimens {
   static const padding1 = 8.0;
 
   /// Size of exercise thumbnails.
-  static const thumbnailSize = 64.0;
+  static const thumbnailSize = 56.0;
 
   /// Aspect ratio of exercise media.
   static const double mediaAspectRatio = 643 / 404;
